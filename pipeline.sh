a28527bfb4b554f0f6ebfe6c70ebb62bc9305961
Ynes <yangyangteng@gmail.com>
2019-07-27 21:37:18 +0100

    new

--- pipeline.sh
+++ pipeline.sh
@@ -1,4 +1,9 @@
+# Set pipeline
 fly -t pipeline-app sp -c chocolate-house/job-chocolate-house.yml -p build-test
+
+# Trigger pipeline job
 fly -t pipeline-app up -p build-test
 fly -t pipeline-app tj -j build-test/job-chocolate-house -w
+
+# List of builds
 fly -t pipeline-app builds
