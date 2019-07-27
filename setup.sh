ef923e599a4f3a1bc59c1619f34f8cfe24b920de
Ynes <yangyangteng@gmail.com>
2019-07-27 21:06:04 +0100

    new

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-fly -t app sp -c chocolate-house/job-chocolate-house.yml -p bulid-test
+fly -t app sp -c chocolate-house/job-chocolate-house.yml -p build-test
 fly -t app up -p build-test
 fly -t app tj -j build-test/job-chocolate-house -w
