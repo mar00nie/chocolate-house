9ad1d4ce2984ef59fa23df2bec83c8ccac2c9e00
Ynes <yangyangteng@gmail.com>
2019-07-27 21:17:01 +0100

    new

--- pipeline.sh
+++ pipeline.sh
@@ -1,4 +1,4 @@
 fly -t app sp -c chocolate-house/job-chocolate-house.yml -p build-test
 fly -t app up -p build-test
 fly -t app tj -j build-test/job-chocolate-house -w
-fly -t tutorial builds
+fly -t app builds
